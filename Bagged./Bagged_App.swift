ffa4f4f83f80593281e7137d7a82f7b1eff0e0c7
Charlotte <charlotteehwang@gmail.com>
2025-07-29 11:29:19 -0700

    Initial Commit

---
+++ Bagged./Bagged_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bagged_App.swift
+//  Bagged.
+//
+//  Created by Charlotte Hwang on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Bagged_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
